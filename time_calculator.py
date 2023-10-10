d24d61a4d6cdb06efe40882561a5049e046a06f9
Marius <mariusc0023@gmail.com>
2023-10-10 21:25:39 +0300

    Update time_calculator.py

--- time_calculator.py
+++ time_calculator.py
@@ -144,7 +144,7 @@ def add_time(startTime, durationTime, startingDay=''):
                     result = str(formatHour) + ":" + minutes + " PM, " + "Thursday (" + str(
                         round(days)) + " days later)"
                 else:
-                    if round(days) != 1:
+                    if round(days) > 2:
                         result = str(formatHour) + ":" + str(minutes) + " PM " + "(" + str(round(days)) + " days later)"
                     else:
                         result = str(formatHour) + ":" + str(minutes) + " PM (next day)"
@@ -191,20 +191,20 @@ def add_time(startTime, durationTime, startingDay=''):
 
 # F
 
-# add_time("3:00 PM", "3:10")
-# # Returns: 6:10 PM
+add_time("3:00 PM", "3:10")
+# Returns: 6:10 PM
 
-add_time("12:30 AM", "14:59", "Monday")
+add_time("11:30 AM", "2:32", "Monday")
 # Returns: 2:02 PM, Monday
 
-# add_time("11:43 AM", "00:20")
-# # Returns: 12:03 PM
+add_time("11:43 AM", "00:20")
+# Returns: 12:03 PM
 
-# add_time("10:10 PM", "3:30")
-# # Returns: 1:40 AM (next day)
-#
-# add_time("11:43 PM", "24:20", "tueSday")
-# # Returns: 12:03 AM, Thursday (2 days later)
-#
-# add_time("6:30 PM", "205:12")
-# # Returns: 7:42 AM (9 days later)
+add_time("10:10 PM", "3:30")
+# Returns: 1:40 AM (next day)
+
+add_time("11:43 PM", "24:20", "tueSday")
+# Returns: 12:03 AM, Thursday (2 days later)
+
+add_time("6:30 PM", "205:12")
+# Returns: 7:42 AM (9 days later)
