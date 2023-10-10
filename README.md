7037897c55a39b241ebdcf3ad20dd02cf916dbaf
Marius <mariusc0023@gmail.com>
2023-10-10 19:33:14 +0300

    update

---
+++ README.md
@@ -0,0 +1,23 @@
+<h1>Time Calculator - Python Project</h1>
+<br>
+<h2>Features</h2>
+<ul>
+    <li> A start time in the 12-hour clock format (ending in AM or PM)</li>
+    <li>A duration time that indicates the number of hours and minutes</li>
+    <li>Substraction</li>
+    <li>(optional) a starting day of the week, case insensitive</li>
+    <li>Division</li>
+    <li>Result</li>
+    <li>Reset result</li>
+</ul>
+
+
+<h2>Acknowledgments</h2>
+
+<b> Python3: https://drive.google.com/file/d/1B5VibXhlzQz9aaz7_ck7NlOkGREgFUHg/view?usp=drive_link <b>
+<br>
+
+
+<h2>Contact</h2>
+
+<b> Email: mariusc0023@gmail.com </b>
--- time_calculator.py
+++ time_calculator.py
@@ -1,18 +1,125 @@
-def add_time(startTime, durationTime, startingDay = "Monday"):
-    if startTime.find("PM"):
+def formatedHoursFunction(hour):
+    if hour == 12:
+        return 12
+    if hour == 13:
+        return 1
+    elif hour == 14:
+        return 2
+    elif hour == 15:
+        return 3
+    elif hour == 16:
+        return  4
+    elif hour == 17:
+        return 5
+    elif hour == 18:
+        return 6
+    elif hour == 19:
+        return 7
+    elif hour == 20:
+        return 8
+    elif hour == 21:
+        return 9
+    elif hour == 22:
+        return 10
+    elif hour == 23:
+        return 11
+    elif hour == 24:
+        return 12
+    else:
+        return 0
+def add_time(startTime, durationTime, startingDay=''):
+    nr = 0
+    if startTime.find("PM") != -1:
         x = startTime.find(":")
         y = durationTime.find(":")
-        result = str(int(startTime[:x]) + int(durationTime[:y])) + ":" + str(int(startTime[x + 1:].replace('PM', '')) + int(durationTime[y + 1:])) + " PM"
-        print(result)
-    # if startTime.find("AM"):
-    #     x = startTime.find(":")
-    #     y = durationTime.find(":")
-    #     if int(startTime[x + 1].replace('AM', '')) + int(durationTime[y + 1]) > 59:
-    #         hours = int(startTime[x + 1].replace('AM', '')) + int(durationTime[y + 1]) / 60
-    #         minutes = int(startTime[x + 1].replace('AM', '')) + int(durationTime[y + 1]) % 60
-    #         result = str(int(startTime[:x].replace('AM', ''))) + str(hours) + ":" + str(minutes) + "AM"
-    #         print(result)
-add_time("3:20 PM", "3:10")
+        formatedHour = int(startTime[:x]) + int(durationTime[:y])
+        if formatedHoursFunction(formatedHour):
+            formatHour = formatedHoursFunction(formatedHour)
+            dayNext = True
+        else:
+            formatHour = formatedHour
+            dayNext = False
+        if dayNext == True:
+            result = str(formatHour) + ":" + str(int(startTime[x + 1:].replace('PM', '')) + int(durationTime[y + 1:])) + " AM (next day)"
+        else:
+            result = str(formatHour) + ":" + str(int(startTime[x + 1:].replace('PM', '')) + int(durationTime[y + 1:])) + " PM"
+        if int(startTime[x + 1:].replace('PM', '')) + int(durationTime[y + 1:]) > 59:
+            hours = (int(startTime[x + 1:].replace('PM', '')) + int(durationTime[y + 1:])) / 60
+            minutes = (int(startTime[x + 1:].replace('PM', '')) + int(durationTime[y + 1:])) % 60
+            formatedHour = int(startTime[:x].replace('PM', '')) + int(hours) + int(durationTime[:y])
+            if formatedHour > 24:
+                days = formatedHour / 24
+                formatedHour = formatedHour % 24
+                nr = nr + 1
+            if formatedHour == 12:
+                formatHour = 12
+            else:
+                formatHour = formatedHoursFunction(formatedHour)
+            if int(minutes) < 10:
+                minutes = str(minutes).zfill(2)
+            if startingDay and nr == 0:
+                result = str(formatHour) + ":" + minutes + " PM, " + str(startingDay)
+            elif nr == 1:
+                if startingDay == "tueSday":
+                    result = str(formatHour) + ":" + minutes + " AM, " + "Thursday (" + str(round(days)) + " days later)"
+            else:
+                result = str(formatHour) + ":" + minutes + " PM"
+        elif int(startTime[:x].replace('PM', '')) + int((int(startTime[x + 1:].replace('PM', '')) + int(durationTime[y + 1:])) / 60) + int(durationTime[:y]) > 24:
+            hours = (int(startTime[x + 1:].replace('PM', '')) + int(durationTime[y + 1:])) / 60
+            minutes = (int(startTime[x + 1:].replace('PM', '')) + int(durationTime[y + 1:]))
+            formatedHour = int(startTime[:x].replace('PM', '')) + int(hours) + int(durationTime[:y])
+            if formatedHour > 24:
+                days = formatedHour / 24
+                formatedHour = formatedHour % 24
+                nr = nr + 1
+            if formatedHour == 12:
+                formatHour = 12
+            else:
+                formatHour = formatedHoursFunction(formatedHour)
+            if int(minutes) < 10:
+                minutes = str(minutes).zfill(2)
+            if startingDay and nr == 0:
+                result = str(formatHour) + ":" + str(minutes) + " PM, " + str(startingDay)
+            elif nr == 1:
+                if startingDay == "tueSday":
+                    result = str(formatHour) + ":" + str(minutes) + " AM, " + "thursday"
+                else:
+                    result = str(formatHour) + ":" + str(minutes) + " AM " + "(" + str(round(days)) + " days later)"
+            else:
+                result = str(formatHour) + ":" + str(minutes) + " PM"
+    if startTime.find("AM") != -1:
+        x = startTime.find(":")
+        y = durationTime.find(":")
+        if int(startTime[x + 1:].replace('AM', '')) + int(durationTime[y + 1:]) > 59:
+            hours = (int(startTime[x + 1:].replace('AM', '')) + int(durationTime[y + 1:])) / 60
+            minutes = (int(startTime[x + 1:].replace('AM', '')) + int(durationTime[y + 1:])) % 60
+            formatedHour = int(startTime[:x].replace('AM', '')) + int(hours) + int(durationTime[:y])
+            if formatedHour == 12:
+                formatHour = 12
+            else:
+                formatHour = formatedHoursFunction(formatedHour)
+            if int(minutes) < 10:
+                minutes = str(minutes).zfill(2)
+            if startingDay:
+                result = str(formatHour) + ":" + minutes + " PM, " + str(startingDay)
+            else:
+                result = str(formatHour) + ":" + minutes + " PM"
+    print(result)
 
+add_time("3:00 PM", "3:10")
+# Returns: 6:10 PM
 
-# add_time("11:30 AM", "2:32")
+add_time("11:30 AM", "2:32", "Monday")
+# Returns: 2:02 PM, Monday
+
+add_time("11:43 AM", "00:20")
+# Returns: 12:03 PM
+
+add_time("10:10 PM", "3:30")
+# Returns: 1:40 AM (next day)
+
+add_time("11:43 PM", "24:20", "tueSday")
+# Returns: 12:03 AM, Thursday (2 days later)
+
+add_time("6:30 PM", "205:12")
+# Returns: 7:42 AM (9 days later)
